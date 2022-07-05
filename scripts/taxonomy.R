ecae354a5db42a28688e74f62c4bbf16a9fa6038
Edwards <edwardsi@ad.unc.edu>
2022-07-05 13:41:06 -0400

    itis script - basic update pull to assess taxize issues from another device

--- scripts/taxonomy.R
+++ scripts/taxonomy.R
@@ -15,15 +15,8 @@ ground_arths <- read_csv(
 
 # rework this to have a dataframe of taxa with itis id's and all taxonomic levels rather than running the whole thing every time
 
-foliage_arths %>% 
-  select(Taxon, TaxonLevel) %>% 
-  unique() %>% 
-  head() %>% 
-  mutate(
-    ITISID = get_ids(sci_com = Taxon, db = 'itis') %>% 
-      str_extract('[0-9]*'))
-
-get_ids(sci_com = c('Melanotus', 'Geometridae'), db = 'itis')$itis %>% classification() %>% 
-  pivot_wider()
-
 tax_name('Phrurotimpus alarius', get = unique(foliage_arths$TaxonLevel))
+
+get_tsn('Phrurotimpus alarius')
+
+classification('Phrurotimpus alarius', db = 'itis')
