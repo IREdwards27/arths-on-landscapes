64c5735cbd95f740f033728806e2cd29b10bbd36
Indigo <iredward27@gmail.com>
2022-07-18 23:01:39 -0400

    success with the ITIS script!

--- scripts/taxonomy.R
+++ scripts/taxonomy.R
@@ -3,6 +3,7 @@
 
 library(taxize)
 library(tidyverse)
+library(lubridate)
 
 foliage_arths <- read_csv(
   list.files('data', full.names = T)[str_detect(list.files('data'), '^foliagearths')])
@@ -14,11 +15,6 @@ ground_arths <- read_csv(
 # retrieve ITIS IDs -------------------------------------------------------
 
 # rework this to have a dataframe of taxa with itis id's and all taxonomic levels rather than running the whole thing every time
-
-input_vector %>%
-  str_replace(., ' ', '\\\\\ ') %>%
-  paste0('nameWOInd:', .) %>%
-  map_df(., ~ritis::itis_search(q = .))
 
 taxa <- tibble(
   taxon = unique(c('Polistes fuscatus', foliage_arths$Taxon[!is.na(foliage_arths$Taxon)], ground_arths$Taxon[!is.na(ground_arths$Taxon)]))) %>% 
@@ -38,5 +34,27 @@ ranks <- map(
       names_from = 'rank',
       values_from = 'name')) %>% 
   bind_rows() %>% 
-  cbind(tsn = taxa$tsn)
-    
+  cbind(
+    tsn = taxa$tsn[!is.na(taxa$tsn)],
+    taxon = taxa$taxon[!is.na(taxa$tsn)]) %>% 
+  select(!kingdom:subphylum) %>% 
+  rbind(taxa[is.na(taxa$tsn),] %>% 
+          mutate(
+            class = NA,
+            subclass = NA,
+            infraclass = NA,
+            order = NA,
+            suborder = NA,
+            superfamily = NA,
+            family = NA,
+            infraorder = NA,
+            subfamily = NA,
+            genus = NA,
+            species = NA,
+            superorder = NA,
+            tribe = NA,
+            subgenus = NA,
+            subtribe = NA)) %>% 
+  relocate(tsn:taxon)
+
+write.csv(ranks, file = paste('data/taxa_', today(), '.csv'), row.names = F)
