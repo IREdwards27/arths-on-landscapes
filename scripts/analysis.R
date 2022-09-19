e6e30c6d9b55f5b1b469e3f46d3a7bc638a5b4ce
Indigo <iredward27@gmail.com>
2022-09-19 13:55:17 -0400

    analysis stuff

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -37,10 +37,13 @@ trees <- read_csv(
 circles <- read_csv(
   list.files('data', full.names = T)[str_detect(list.files('data'), '^circles')])
 
+sites <- read_csv(
+  list.files('data', full.names = T)[str_detect(list.files('data'), '^sites')])
 
-# foliage dataframe processing (site level) -------------------------------------
 
-# calculate total number of observations for each class (Collembola = 39, Euchelicerata = 371, Insecta = 2942)
+# taxon composition assessment -------------------------------------
+
+# calculate total number of observations for each class of foliage arths
 foliage_arths %>% 
   filter(!is.na(TaxonID)) %>% 
   left_join(
@@ -49,20 +52,45 @@ foliage_arths %>%
   group_by(class) %>% 
   summarize(number_observed = sum(Quantity, na.rm = T))
 
-# calculate total number of observations for each order
+# calculate total number of observations of foliage arths by order
 foliage_arths %>% 
   filter(!is.na(TaxonID)) %>% 
   left_join(
     taxa,
     by = 'TaxonID') %>% 
+  filter(!is.na(family)) %>% 
   group_by(order) %>% 
   summarize(number_observed = sum(Quantity, na.rm = T))
 
-# calculate total family diversity observed at each site (not standardized by number of surveys)
-foliage_arths %>% 
+# calculate total number of observations for each class for ground arths
+ground_arths %>% 
+  filter(!is.na(TaxonID)) %>% 
   left_join(
     taxa,
     by = 'TaxonID') %>% 
+  group_by(class) %>% 
+  summarize(number_observed = sum(Number, na.rm = T))
+
+# calculate total number of observations for each order for ground arths
+ground_arths %>% 
+  filter(!is.na(TaxonID)) %>% 
+  left_join(
+    taxa,
+    by = 'TaxonID') %>% 
+  filter(!is.na(family)) %>% 
+  group_by(order) %>% 
+  summarize(number_observed = sum(Number, na.rm = T))
+
+
+# setting up analysis dataframes ------------------------------------------
+
+# foliage arth site-level dataframe with family diversity and mean arthropod biomass per survey
+foliage_sites <- foliage_arths %>% 
+  left_join(
+    taxa,
+    by = 'TaxonID') %>% 
+  filter(
+    order %in% c('Araneae','Coleoptera','Hemiptera','Opiliones','Orthoptera') | (order == 'Hymenoptera' & family == 'Formicidae')) %>% 
   left_join(
     beatsheets %>% 
       select(BeatSheetID, TreeFK),
@@ -76,38 +104,47 @@ foliage_arths %>%
       select(CircleID, SiteFK),
     by = c('CircleFK' = 'CircleID')) %>% 
   group_by(SiteFK) %>% 
-  summarize(fam_div = n_distinct(family))
+  summarize(fam_div = n_distinct(family)) %>% 
+  left_join(
+    foliage_arths %>% 
+      left_join(
+        taxa,
+        by = 'TaxonID') %>% 
+      filter(
+        order %in% c('Araneae','Coleoptera','Hemiptera','Opiliones','Orthoptera') | (order == 'Hymenoptera' & family == 'Formicidae')) %>% 
+      group_by(BeatSheetFK) %>% 
+      summarize(mass_per_survey = sum(TotalMass, na.rm = T)) %>% 
+      right_join(
+        beatsheets %>% 
+          select(BeatSheetID, TreeFK),
+        by = c('BeatSheetFK' = 'BeatSheetID')) %>% 
+      mutate(mass_per_survey = if_else(
+        is.na(mass_per_survey),
+        true = 0,
+        false = mass_per_survey)) %>% 
+      left_join(
+        trees %>% 
+          select(TreeID, CircleFK),
+        by = c('TreeFK' = 'TreeID')) %>% 
+      left_join(
+        circles %>% 
+          select(CircleID, SiteFK),
+        by = c('CircleFK' = 'CircleID')) %>% 
+      group_by(SiteFK) %>% 
+      summarize(mean_mass_per_survey = mean(mass_per_survey, na.rm = T)),
+    by = 'SiteFK') %>% 
+  left_join(
+    sites %>% 
+      select(c(SiteID, forest_2km, pafrac_5km, nlsi_2km, nlsi_5km:mn_night_temp)),
+    by = c('SiteFK' = 'SiteID'))
 
-# calculate abundance as mean biomass per survey (not standardized for missing values)
-foliage_arths %>% 
+# ground arth site-level dataframe with family diversity and mean arthropod biomass per survey
+ground_sites <- ground_arths %>% 
   left_join(
     taxa,
     by = 'TaxonID') %>% 
-  group_by(BeatSheetFK) %>% 
-  summarize(mass_per_survey = sum(TotalMass, na.rm = T)) %>% 
-  left_join(
-    beatsheets %>% 
-      select(BeatSheetID, TreeFK),
-    by = c('BeatSheetFK' = 'BeatSheetID')) %>% 
-  left_join(
-    trees %>% 
-      select(TreeID, CircleFK),
-    by = c('TreeFK' = 'TreeID')) %>% 
-  left_join(
-    circles %>% 
-      select(CircleID, SiteFK),
-    by = c('CircleFK' = 'CircleID')) %>% 
-  group_by(SiteFK) %>% 
-  summarize(mean_mass_per_survey = mean(mass_per_survey, na.rm = T))
-
-
-# ground dataframe processing (site level) -------------------------------------
-
-# calculate family diversity (not standardized)
-ground_arths %>% 
-  left_join(
-    taxa,
-    by = 'TaxonID') %>%
+  filter(
+    order %in% c('Araneae','Archaeognatha','Coleoptera','Isopoda', 'Opiliones', 'Orthoptera') | (order == 'Hymenoptera' & family == 'Formicidae')) %>% 
   left_join(
     pitfalls,
     by = 'PitfallID') %>% 
@@ -115,47 +152,62 @@ ground_arths %>%
     circles,
     by = 'CircleID') %>% 
   group_by(SiteFK) %>% 
-  summarize(fam_div = n_distinct(family))
+  summarize(fam_div = n_distinct(family)) %>% 
+  left_join(
+    ground_arths %>% 
+      left_join(
+        taxa,
+        by = 'TaxonID') %>% 
+      filter(
+        order %in% c('Araneae','Archaeognatha','Coleoptera','Isopoda', 'Opiliones', 'Orthoptera') | (order == 'Hymenoptera' & family == 'Formicidae')) %>%
+      group_by(PitfallID) %>% 
+      summarize(mass_per_trap = sum(TotalMass, na.rm = T)) %>% 
+      left_join(
+        pitfalls,
+        by = 'PitfallID') %>% 
+      left_join(
+        circles,
+        by = 'CircleID') %>% 
+      group_by(SiteFK) %>% 
+      summarize(mean_mass_per_trap = mean(mass_per_trap, na.rm = T)),
+    by = 'SiteFK') %>% 
+  left_join(
+    sites %>% 
+      select(c(SiteID, forest_2km, pafrac_5km, nlsi_2km, nlsi_5km:mn_night_temp)),
+    by = c('SiteFK' = 'SiteID'))
 
-# calculate abundance as # of arths per trap (not standardized)
-ground_arths %>% 
-  left_join(
-    taxa,
-    by = 'TaxonID') %>%
-  group_by(PitfallID) %>% 
-  summarize(arths_per_trap = sum(Number, na.rm = T)) %>% 
-  left_join(
-    pitfalls,
-    by = 'PitfallID') %>% 
-  left_join(
-    circles,
-    by = 'CircleID') %>% 
-  group_by(SiteFK) %>% 
-  summarize(mean_arths_per_trap = mean(arths_per_trap, na.rm = T))
 
-# calculate abundance as biomass per trap (not standardized)
-ground_arths %>% 
-  left_join(
-    taxa,
-    by = 'TaxonID') %>%
-  group_by(PitfallID) %>% 
-  summarize(mass_per_trap = sum(TotalMass, na.rm = T)) %>% 
-  left_join(
-    pitfalls,
-    by = 'PitfallID') %>% 
-  left_join(
-    circles,
-    by = 'CircleID') %>% 
-  group_by(SiteFK) %>% 
-  summarize(mean_mass_per_trap = mean(mass_per_trap, na.rm = T))
+# modeling ----------------------------------------------------------------
+
+ground_abundance_model <- lm(
+  mean_mass_per_trap ~ forest_2km + pafrac_5km + nlsi_2km + mn_day_temp,
+  data = ground_sites)
+
+summary(ground_abundance_model)
+
+ground_diversity_model <- lm(
+  fam_div ~ forest_2km + pafrac_5km + nlsi_2km + mn_day_temp,
+  data = ground_sites)
+
+summary(ground_diversity_model)
+
+foliage_abundance_model <- lm(
+  mean_mass_per_survey ~ forest_2km + pafrac_5km + nlsi_2km + mn_day_temp,
+  data = foliage_sites)
+
+summary(foliage_abundance_model)
+
+foliage_diversity_model <- lm(
+  fam_div ~ forest_2km + pafrac_5km + nlsi_2km + mn_day_temp,
+  data = foliage_sites)
+
+summary(foliage_diversity_model)
 
 
 # next steps --------------------------------------------------------------
 
-# join arth stats to landscape stats
-# join ground arth stats to circle stats
-# join foliage arth stats to tree stats?
-# review model possibilities for site-level and circle-level modeling - notes from ENEC 562
+# separate abundance and diversity frames - abundance should be one row for the mass in each survey so modeling calculates mean, total diversity cannot be - how do we handle this?
+# review model possibilities - notes from ENEC 562
 ## for site-level, what can best account for small sample size (6 replicates)?
 ## predictor variable selection using what type of model selection?
 ## start with GLM and go from there?
--- scripts/hobo_processing.R
+++ scripts/hobo_processing.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(lubridate)
 
 all_data <- map2_dfr(
   list.files('data/hobo_data', full.names = T)[c(1:5,7)],
@@ -12,3 +13,30 @@ all_data <- map2_dfr(
     datetime = lubridate::mdy_hms(datetime, tz = Sys.timezone()))
 
 write.csv(all_data, 'data/temperatures.csv')
+
+temps <- read_csv('data/temperatures.csv') %>% 
+  mutate(datetime = mdy_hm(datetime)) %>% 
+  mutate(
+    daynight = if_else(
+      hour(datetime) %in% c(0:6,21:24),
+      true = 'night',
+      false = 'day'))
+
+sites <- read_csv(
+  list.files('data', full.names = T)[str_detect(list.files('data'), '^sites')])
+
+site_temps <- temps %>% 
+  group_by(site, daynight) %>% 
+  summarize(mean_temp = mean(tempC)) %>% 
+  pivot_wider(
+    names_from = daynight,
+    values_from = mean_temp) %>% 
+  rename(
+    mn_day_temp = day,
+    mn_night_temp = night) %>% 
+  left_join(
+    sites,
+    by = c('site' = 'SiteID'))
+
+write.csv(site_temps, paste('data/sites_', today(), '.csv'))
+
